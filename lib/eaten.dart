7c414974e2f32818d39a289652d4379919ba6c6c
Ashiya Kandhway <ashiyakandhway27july@gmail.com>
2021-07-25 17:36:41 +0530

    initial commit

---
+++ lib/eaten.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+class Myfood extends StatelessWidget {
+  final innercolor;
+  final outercolor;
+  final child;
+
+  Myfood({this.innercolor,this.outercolor, this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(1.0),
+      child:ClipRRect(
+        borderRadius: BorderRadius.circular(6),
+        child: Container(
+          padding: EdgeInsets.all(12),
+          color: outercolor,
+          child: Center(child: ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Container(
+              color: innercolor,
+              child: Center(child: child),
+
+            ),
+          ),
+          ),
+        ),
+      ),
+    );
+  }
+}
